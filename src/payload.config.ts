7fa0c652ee61ecaca8cc9837f10e6b14ada510e1
wanghui <wanghuinet@gmail.com>
2026-09-12 19:39:26 +0800

    feat: configure Payload for D1 and R2

---
+++ src/payload.config.ts
@@ -0,0 +1,40 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { sqliteD1Adapter } from '@payloadcms/db-d1-sqlite'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { r2Storage } from '@payloadcms/storage-r2'
+import { buildConfig } from 'payload'
+import { CloudflareContext, getCloudflareContext } from '@opennextjs/cloudflare'
+import { GetPlatformProxyOptions } from 'wrangler'
+import { Users } from './collections/Users'
+import { Media } from './collections/Media'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+const realpath = (value: string) => (fs.existsSync(value) ? fs.realpathSync(value) : undefined)
+const isCLI = process.argv.some((value) => realpath(value)?.endsWith(path.join('payload', 'bin.js')))
+const isProduction = process.env.NODE_ENV === 'production'
+
+const cloudflare = isCLI || !isProduction
+  ? await getCloudflareContextFromWrangler()
+  : await getCloudflareContext({ async: true })
+
+export default buildConfig({
+  admin: { user: Users.slug, importMap: { baseDir: path.resolve(dirname) } },
+  collections: [Users, Media],
+  editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: { outputFile: path.resolve(dirname, 'payload-types.ts') },
+  db: sqliteD1Adapter({ binding: cloudflare.env.D1 }),
+  storage: [r2Storage({ bucket: cloudflare.env.R2, collections: { media: true } })],
+})
+
+function getCloudflareContextFromWrangler(): Promise<CloudflareContext> {
+  return import(/* webpackIgnore: true */ `${'__wrangler'.replaceAll('_', '')}`).then(
+    ({ getPlatformProxy }) => getPlatformProxy({
+      environment: process.env.CLOUDFLARE_ENV,
+      remoteBindings: isProduction,
+    } satisfies GetPlatformProxyOptions),
+  )
+}
