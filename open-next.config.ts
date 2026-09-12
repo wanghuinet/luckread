db894b675dc787f9b1ea2560e2686b0e82d176ea
wanghui <wanghuinet@gmail.com>
2026-09-13 04:41:25 +0800

    fix: use OpenNext Cloudflare config helper

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,27 +1,3 @@
-import type { OpenNextConfig } from '@opennextjs/cloudflare'
+import { defineCloudflareConfig } from '@opennextjs/cloudflare/config'
 
-const config: OpenNextConfig = {
-  default: {
-    override: {
-      wrapper: 'cloudflare-node',
-      converter: 'edge',
-      proxyExternalRequest: 'fetch',
-      incrementalCache: 'dummy',
-      tagCache: 'dummy',
-      queue: 'dummy',
-    },
-  },
-  middleware: {
-    external: true,
-    override: {
-      wrapper: 'cloudflare-edge',
-      converter: 'edge',
-      proxyExternalRequest: 'fetch',
-      incrementalCache: 'dummy',
-      tagCache: 'dummy',
-      queue: 'dummy',
-    },
-  },
-}
-
-export default config
+export default defineCloudflareConfig({})
