a582a1bd78e312e3facc6cc46dd73d22c2d659e1
wanghui <wanghuinet@gmail.com>
2026-09-13 06:15:27 +0800

    fix: externalize @next/env for OpenNext Cloudflare build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 const nextConfig = {
   reactStrictMode: true,
-  serverExternalPackages: ['@payloadcms/db-d1-sqlite', 'jose'],
+  serverExternalPackages: ['@payloadcms/db-d1-sqlite', 'jose', '@next/env'],
 }
 
 export default withPayload(nextConfig)
