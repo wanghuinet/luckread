e34e18c5a0e13759503b8ac9636e419e491291d8
wanghui <wanghuinet@gmail.com>
2026-09-21 08:42:37 +0800

    chore(W01): register AUTH-002 session migration

--- workers/W01-payload/src/migrations/index.ts
+++ workers/W01-payload/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250929_111647 from './20250929_111647'
+import * as migration_20260921_003203_MIG_AUTH_002_SESSION_V1 from './20260921_003203_MIG_AUTH_002_SESSION_V1'
 
 export const migrations = [
   {
     up: migration_20250929_111647.up,
     down: migration_20250929_111647.down,
     name: '20250929_111647',
+  },
+  {
+    up: migration_20260921_003203_MIG_AUTH_002_SESSION_V1.up,
+    down: migration_20260921_003203_MIG_AUTH_002_SESSION_V1.down,
+    name: '20260921_003203_MIG_AUTH_002_SESSION_V1',
   },
 ]
