da1b0acad09d1cd5d00c104cb29bb932f49ac55e
wanghui <wanghuinet@gmail.com>
2026-09-25 19:33:22 +0800

    feat(auth): register ENT-USER profile migration

--- workers/W01-payload/src/migrations/index.ts
+++ workers/W01-payload/src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250929_111647 from './20250929_111647'
 import * as migration_20260921_003203_MIG_AUTH_002_SESSION_V1 from './20260921_003203_MIG_AUTH_002_SESSION_V1'
+import * as migration_20260925_111503_MIG_ENT_USER_PROFILE_V1 from './20260925_111503_MIG_ENT_USER_PROFILE_V1'
 
 export const migrations = [
   {
@@ -11,5 +12,10 @@ export const migrations = [
     up: migration_20260921_003203_MIG_AUTH_002_SESSION_V1.up,
     down: migration_20260921_003203_MIG_AUTH_002_SESSION_V1.down,
     name: '20260921_003203_MIG_AUTH_002_SESSION_V1',
+  },
+  {
+    up: migration_20260925_111503_MIG_ENT_USER_PROFILE_V1.up,
+    down: migration_20260925_111503_MIG_ENT_USER_PROFILE_V1.down,
+    name: '20260925_111503_MIG_ENT_USER_PROFILE_V1',
   },
 ]
